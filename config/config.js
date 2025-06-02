4ebdeed24e702a06e68f36b7d842670454622799
jvitor5555 <jvfelismino03@gmail.com>
2025-06-01 23:10:07 -0300

    config

--- config/config.js
+++ config/config.js
@@ -2,15 +2,15 @@ const { Sequelize } = require('sequelize');
 
 require('dotenv').config();
  
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD, {
-        host: process.env.DB_HOST,
-        dialect: 'postgres',
-        logging: false
-    }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // cuidado, é inseguro em produção sem configuração correta
+        },
+    },
+});
 
 async function conectarBanco() {
     
